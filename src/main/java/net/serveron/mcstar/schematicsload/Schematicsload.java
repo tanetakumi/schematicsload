902127189571f549dfd78cd7c3ade31de316dbf3
tanetakumi <nutantakumi@yahoo.co.jp>
2020-12-13 19:15:08 +0900

    first commit

---
+++ src/main/java/net/serveron/mcstar/schematicsload/Schematicsload.java
@@ -0,0 +1,17 @@
+package net.serveron.mcstar.schematicsload;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Schematicsload extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
